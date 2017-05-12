d03da2378bb855a6c3f37a0b10f525d0102cf0a5
moomoofarm3 <michaelchiu@Michaels-MacBook-Pro.local>
2017-05-12 13:08:42 -0700

    Login Paths and htmlcss

---
+++ public/js/StockInfoApp/Components/user/user.js
@@ -0,0 +1,9 @@
+var login_user = {
+  template: '<div class="form">\
+            <input type="text" class="zocial-dribbble" placeholder="Username" />\
+            <input type="text" placeholder="Password" />\
+            <input type="submit" value="Login" />\
+            <a href="">Lost your password?</a>\
+        </div> \
+'
+};
--- public/js/StockInfoApp/stockinfo.js
+++ public/js/StockInfoApp/stockinfo.js
@@ -5,6 +5,7 @@
 const routes = [
   { path: '/Portfolio', component: portfolio_page },
   { path: '/Trade', component: trade_page, props: { gameId: 1} }
+  { path: '/login_user', component: login_user}
 ]
 
 const router = new VueRouter({
