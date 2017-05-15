c97b361633434c1119e2cc3e282c1f931804bf03
Travis Williams <btraviswilliams@gmail.com>
2017-05-15 09:57:01 -0700

    fixed a couple bugs and updates some styles

--- public/js/StockInfoApp/Components/user/login.js
+++ public/js/StockInfoApp/Components/user/login.js
@@ -18,13 +18,25 @@ Vue.component('login-form', {
 					this.message = 'Failed to login';
 				}
 			});
+		},
+		register() {
+			var userapi = new UserAPI().RegisterUser(this.username, this.password).then((result) => {
+				if(result){
+					this.message = 'Success';
+					this.failed = false;
+				} else {
+					this.failed = true;
+					this.message = 'Failed to login';
+				}
+			});
 		}
 	},
 	template: '<div class="login">\
 				<input type="text" v-model="username" placeholder="Username" />\
 				<input type="password" v-model="password" placeholder="Password" />\
 				<input type="submit" value="Login" v-on:click="login()"/>\
-				<a href="">Lost your password?</a>\
+				<input type="submit" value="Register" v-on:click="register()"/>\
+				<a href="" class="floated right">Lost your password?</a>\
 				{{ message }}\
 			</div>'
 });
